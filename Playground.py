09d3ec7f2a721dbbe57dffe00bdd27374acecf24
Sean McOwen <SeanMcOwen@gmail.com>
2020-10-06 07:01:18 -0400

    Goal streaks

--- Playground.py
+++ Playground.py
@@ -14,8 +14,8 @@ database_name = 'Goals.db'
 
 from datetime import datetime
 with sqlite3.connect(database_name) as conn:
-    #goals, work_log = RedBook.Data.process_goals_SQL(conn)
-    #expected_progress_table = RedBook.Tables.build_expected_progress_table(goals)
+    goals, work_log = RedBook.Data.process_goals_SQL(conn)
+    expected_progress_table = RedBook.Tables.build_expected_progress_table(goals)
     #expected_work_table = RedBook.Tables.build_expected_work_table(goals)
     
     #expected_progress_table, expected_work_table, expected_work_tables = RedBook.Tables.build_expected_work_tables(goals)
@@ -23,10 +23,13 @@ with sqlite3.connect(database_name) as conn:
     #habits = RedBook.Data.pull_habits_data_SQL(conn)
     #habits_progress = RedBook.Data.pull_habits_log_SQL(conn)
     
-    habits, progress = RedBook.Data.process_habits_SQL(conn)
+    #habits, progress = RedBook.Data.process_habits_SQL(conn)
     #table = RedBook.Tables.build_expected_progress_table_habits(habits)
     #["Daily","Weekly","Monthly","Quarterly","Yearly"]
     #tasks = RedBook.Data.pull_tasks_SQL(conn)
 
     #RedBook.Data.check_goal_completion(conn, goals)
-    print(habits['Object'].apply(lambda x: x.streak))
+
+    group = RedBook.Classes.Group(goals.iloc[0:1])
+    cal_choice = 'Week'
+    data = group.build_group_schedule(cal_choice, data_type = 'Completion')
