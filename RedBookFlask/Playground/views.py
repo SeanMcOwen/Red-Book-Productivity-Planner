9e8f682e1d0d058bb599d83b58349acd9a83ff2f
Sean McOwen <SeanMcOwen@gmail.com>
2020-11-10 13:13:38 -0500

    task increments data table base

--- RedBookFlask/Playground/views.py
+++ RedBookFlask/Playground/views.py
@@ -16,4 +16,44 @@ playground_blueprint = Blueprint('playground',
 
 @playground_blueprint.route("/DataTables",methods=['GET', 'POST'])
 def data_tables_page():
-    return render_template("DataTables.html")
+    with sqlite3.connect(database_name) as conn:
+        tasks = RedBook.Data.pull_tasks_SQL(conn)
+    
+    tables = ""
+    for col in ["Today","Week","Month","Quarter","Year"]:
+        temp = tasks[tasks[col]][['Task Name', 'Due Date']]
+        if len(temp) > 0:
+            tables += "<h3>"+col+"</h3>"
+            tables += temp.to_html(index=False)
+            tables += "<br>"
+        else:
+            continue
+    temp = tasks[['Task Name', 'Due Date']]
+    tables += "<h3>All Tasks</h3>"
+    tables += temp.to_html(index=False)
+    tables += "<br>"
+    
+    tables = ""
+    tables += """
+    <table id="today" class="display">
+    <thead>
+        <tr>
+            <th>Task Name</th>
+            <th>Due Date</th>
+        </tr>
+    </thead>
+    <tbody>
+    """
+    for vals in temp.values:
+        tables +=  """
+        <tr>
+            <td>{}</td>
+            <td>{}</td>
+        </tr>
+        """.format(vals[0], vals[1])
+    tables +=  """</tbody>
+</table>"""
+        
+
+    
+    return render_template("DataTables.html", tables=tables)
