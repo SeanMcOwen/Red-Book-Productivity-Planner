8a94f9a1eb36498ab028d4ea2ced142f500ddb16
Sean McOwen <SeanMcOwen@gmail.com>
2020-11-25 21:51:07 -0500

    New format

--- Playground.py
+++ Playground.py
@@ -11,7 +11,7 @@ database_name = 'Goals.db'
 
 from datetime import datetime
 with sqlite3.connect(database_name) as conn:
-    existing_progress = list(pd.read_sql("SELECT * FROM progress", conn)['Goal Name'].unique())
+    #existing_progress = list(pd.read_sql("SELECT * FROM progress", conn)['Goal Name'].unique())
     #goals, work_log = RedBook.Data.process_goals_SQL(conn)
     #expected_progress_table, expected_work_table, percent_left_table, expected_work_tables = RedBook.Tables.build_expected_work_tables(goals)
     #RedBook.Data.filter_increment_hiding(goals, expected_work_tables)
@@ -26,8 +26,8 @@ with sqlite3.connect(database_name) as conn:
     #habits = RedBook.Data.pull_habits_data_SQL(conn)
     #habits_progress = RedBook.Data.pull_habits_log_SQL(conn)
     
-    #habits, progress = RedBook.Data.process_habits_SQL(conn)
-    #table = RedBook.Tables.build_expected_progress_table_habits(habits)
+    habits, progress = RedBook.Data.process_habits_SQL(conn)
+    table = RedBook.Tables.build_expected_progress_table_habits(habits)
     #["Daily","Weekly","Monthly","Quarterly","Yearly"]
     #tasks = RedBook.Data.pull_tasks_SQL(conn)
 
