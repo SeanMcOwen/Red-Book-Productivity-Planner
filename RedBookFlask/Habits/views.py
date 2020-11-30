9e8ff79489a056715e6c9131eebd8ad31afd5773
Sean McOwen <SeanMcOwen@gmail.com>
2020-11-30 10:20:01 -0500

    habit streaks page

--- RedBookFlask/Habits/views.py
+++ RedBookFlask/Habits/views.py
@@ -17,8 +17,29 @@ habits_blueprint = Blueprint('habits',
 
 @habits_blueprint.route("/Streaks",methods=['GET', 'POST'])
 def streaks_page():
-    return render_template("Habits.html", template="Flask")
+    with sqlite3.connect(database_name) as conn:
+        if not RedBook.Data.check_table_exists(conn, 'habits'):
+            error = "<h3>Please create a habit.</h3>"
+            return render_template("ErrorPage.html", error=error,
+                                   template="Flask")
+        habits, progress = RedBook.Data.process_habits_SQL(conn)
+    tables = RedBook.Tables.create_streak_tables(habits)
+    
+    tables_html = ""
+    for key in tables.keys():
+        tables_html += "<h3>{}</h3><br>".format(key)
+        tables_html += tables[key].to_html(index=False)
+        tables_html+="<br><br>"
+        
+    return render_template("Habits.html", tables=tables_html, template="Flask")
 
 @habits_blueprint.route("/Habits",methods=['GET', 'POST'])
 def habits_page():
+    with sqlite3.connect(database_name) as conn:
+        if not RedBook.Data.check_table_exists(conn, 'habits'):
+            error = "<h3>Please create a habit.</h3>"
+            return render_template("ErrorPage.html", error=error,
+                                   template="Flask")
+        habits, progress = RedBook.Data.process_habits_SQL(conn)
+    
     return render_template("Streaks.html", template="Flask")
