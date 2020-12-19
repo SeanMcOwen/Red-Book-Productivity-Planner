7dfb5378d4ca0bb5d92e385ae4f9cf73227fa203
Sean McOwen <SeanMcOwen@gmail.com>
2020-12-19 12:49:35 -0500

    complete habits button

--- RedBookFlask/Habits/views.py
+++ RedBookFlask/Habits/views.py
@@ -3,6 +3,7 @@ from bokeh.embed import server_document
 import sqlite3
 import RedBook
 import pandas as pd
+from datetime import datetime
 
 database_name = 'Goals.db'
 
@@ -48,7 +49,20 @@ def habits_page():
             habit_name  = request.values.get('habit2')
             habit_names = list(habits['Habit Name'].values)
             habit_names.pop(habit_names.index(habit_name))
-            print(habit_name)
+            conn.cursor().execute("UPDATE habits SET Completed = 'Completed' WHERE `Habit Name` = '{}'".format(habit_name))
+            conn.commit()
+            
+            df = pd.read_sql("SELECT * FROM habits", conn)
+            df['Start Date'] = pd.to_datetime(df['Start Date'])
+            df.to_csv("Habits.csv")
+            
+            completed = pd.DataFrame([[habit_name, "Habit", pd.to_datetime(datetime.today().date())]],
+                                 columns = ['Name', 'Type', 'Date'])
+            completed.to_sql("Completed", conn, if_exists='append', index=False)
+            df = pd.read_sql("SELECT * FROM Completed", conn)
+            df['Date'] = pd.to_datetime(df['Date'])
+            df.to_csv("Completed.csv", index=False)
+
             return render_template("Habits.html", habits=habit_names,template="Flask")
 
         else:
